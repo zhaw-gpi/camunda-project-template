b2073d18e1e997c7341d7c2596cd1e814c382699
Peter Heinrich <peter.heinrich@its-heinrich.ch>
2018-11-06 13:16:18 +0100

    initial project commit

---
+++ src/main/java/ch/zhaw/sml/iwi/gpi/blankproject/BlankProjectStarter.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.zhaw.sml.iwi.gpi.blankproject;
+
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author Peter Heinrich <heip@zhaw.ch>
+ */
+@SpringBootApplication
+@EnableProcessApplication
+public class BlankProjectStarter {
+    
+    public static void main(String[] args) {
+                SpringApplication.run(BlankProjectStarter.class, args);
+    }
+}
